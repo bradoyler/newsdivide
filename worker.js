d207fe6897798211a081d09f32df3759704d3f49
Brad Oyler <bradoyler@gmail.com>
2016-11-14 20:52:25 -0500

    update cron

--- worker.js
+++ worker.js
@@ -6,24 +6,19 @@ var manifest = require('./lib/manifest.json');
 
 console.log('starting CRON...');
 
-var jobA = new CronJob({
-  cronTime: '1 20 10-20 * * *',
-  onTick: function () {
-    var options = {
-      defaultWhiteBackground: true,
-      errorIfStatusIsNot200: true,
-      timeout: 65000,
-      quality: 95,
-      streamType: 'jpg',
-      renderDelay: 1800,
-      screenSize: { width: 375, height: 667 },
-      userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
-    };
+var jobA = new CronJob('1 2 10-20 * * *', function () {
+  var options = {
+    defaultWhiteBackground: true,
+    errorIfStatusIsNot200: true,
+    timeout: 65000,
+    quality: 95,
+    streamType: 'jpg',
+    renderDelay: 1800,
+    screenSize: { width: 375, height: 667 },
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+  };
 
-    capture.forEachWebShot(manifest.pages, options);
-  },
-  start: false,
-  timeZone: 'America/New_York'
-});
+  capture.forEachWebShot(manifest.pages, options);
+}, null, true, 'America/New_York');
 
-jobA.start();
+//jobA.start();
