d9adc3da69c35aab23ce241ad7316e4dde824b0e
Brad Oyler <bradoyler@gmail.com>
2016-11-16 11:13:58 -0500

    update cron

--- worker.js
+++ worker.js
@@ -5,7 +5,9 @@ var CronJob = require('cron').CronJob;
 var capture = require('./lib/capture');
 var manifest = require('./lib/manifest.json');
 const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
-const cronScheduleA = '1 2 1,6,9,12,14,16,18,20 * * *';
+const cronScheduleA = '1 2 1,6 * * *';
+const cronScheduleB = '1 2 9-18,20 * * *';
+const cronScheduleC = '1 2 20 * * *';
 
 const defaults = {
   defaultWhiteBackground: true,
@@ -21,6 +23,16 @@ const defaults = {
 console.log('starting up CRON jobs...');
 
 var jobA = new CronJob(cronScheduleA, function () {
+  var options = defaults;
+  capture.forEachWebShot(manifest.pages, options);
+}, null, true, 'America/New_York');
+
+var jobB = new CronJob(cronScheduleB, function () {
+  var options = defaults;
+  capture.forEachWebShot(manifest.pages, options);
+}, null, true, 'America/New_York');
+
+var jobC = new CronJob(cronScheduleC, function () {
   var options = defaults;
   capture.forEachWebShot(manifest.pages, options);
 }, null, true, 'America/New_York');
