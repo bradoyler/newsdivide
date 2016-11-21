3661ae397831104c9babe18aa4ba9e954542b852
Brad Oyler <bradoyler@gmail.com>
2016-11-21 10:10:29 -0500

    manifest clone

--- worker.js
+++ worker.js
@@ -3,7 +3,8 @@ var dotenv = require('dotenv');
 dotenv.config({silent: true});
 var CronJob = require('cron').CronJob;
 var capture = require('./lib/capture');
-var manifest = require('./lib/manifest.json');
+var manifestCache = require('./lib/manifest.json');
+var manifest = JSON.parse(JSON.stringify(manifestCache));
 const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
 const cronScheduleA = '1 2 1,6 * * *';
 const cronScheduleB = '1 2 9-18 * * *';
@@ -25,19 +26,23 @@ console.log('starting...');
 var jobA = new CronJob(cronScheduleA, function () {
   console.log('>>> cronA', new Date());
   var options = defaults;
+  manifest = JSON.parse(JSON.stringify(manifestCache));
   capture.forEachWebShot(manifest.pages, options);
 }, null, true, 'America/New_York');
 
 var jobB = new CronJob(cronScheduleB, function () {
   console.log('>>> cronB', new Date());
   var options = defaults;
+  manifest = JSON.parse(JSON.stringify(manifestCache));
   capture.forEachWebShot(manifest.pages, options);
 }, null, true, 'America/New_York');
 
 var jobC = new CronJob(cronScheduleC, function () {
   console.log('>>> cronC', new Date());
   var options = defaults;
+  manifest = JSON.parse(JSON.stringify(manifestCache));
   capture.forEachWebShot(manifest.pages, options);
 }, null, true, 'America/New_York');
 
+// first run...
 capture.forEachWebShot(manifest.pages, defaults);
