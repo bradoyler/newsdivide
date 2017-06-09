ba4ca1c2f8296381c7e337ee7dfdb092837a4b33
Brad Oyler <bradoyler@gmail.com>
2017-06-09 10:47:58 -0400

    fixes

--- test/index.js
+++ test/index.js
@@ -3,9 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config({silent: true});
 const CronJob = require('cron').CronJob;
 const capture = require('../lib/capture');
-const manifest = require('../lib/test-manifest.json');
+const testManifest = require('../lib/test-manifest.json');
 const debug = require('debug')('test');
 const cronScheduleB = '1 34 9-18 * * *';
+
+const mobileAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1';
+const desktopAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
 
 const options = {
   defaultWhiteBackground: true,
@@ -16,11 +19,11 @@ const options = {
   streamType: 'jpg',
   renderDelay: 200,
   screenSize: { width: 375, height: 667 },
-  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+  userAgent: desktopAgent
 };
 
-debug('defaults ~', options);
-capture.forEachWebShot(manifest.pages, options);
+// debug('defaults ~', options);
+capture.forEachWebShot(testManifest.pages, options);
 
 const jobB = new CronJob(cronScheduleB, function () {
   debug('>>> cron', new Date());
