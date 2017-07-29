ca82e869b534b718f448b01047893d0a3f5ccd16
Brad Oyler <bradoyler@gmail.com>
2017-07-29 00:01:18 -0400

    fix PORT

--- web.js
+++ web.js
@@ -13,11 +13,12 @@ app.post('/webshot/:url', function (req, res) {
     body
   });
 });
+app.set('port', process.env.PORT || 3000);
 
-app.listen(3000, function (err) {
+app.listen(app.get('port'), function (err) {
   if (err) {
     throw err;
   }
 
-  console.log('Server started on port 3000');
+  console.log('Server started on port' + app.get('port'));
 });
