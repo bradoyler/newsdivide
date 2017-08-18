eb4213872976d6b82f1b491d551fd4e4e7c125d7
Brad Oyler <bradoyler@gmail.com>
2017-08-17 23:21:34 -0400

    use puppeteer

---
+++ test/puppet.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+const defaults = {
+  type: 'jpeg',
+  quality: 70,
+  fullPage: false
+};
+
+puppeteer.launch().then(async browser => {
+  const page = await browser.newPage();
+  await page.setViewport({ width: 375, height: 667, isMobile: true });
+  await page.goto('https://www.nbcnews.com/mach');
+  await page.injectFile('lib/hideEls.js');
+  defaults.path = 'test/www.nbcnews.mach.jpg';
+  await page.screenshot(defaults);
+
+  // await page.goto('https://www.nbcnews.com');
+  // await page.injectFile('lib/hideEls.js');
+  // defaults.path = 'test/www.nbcnews.com.jpg';
+  // await page.screenshot(defaults);
+  //
+  // await page.goto('http://m.huffpost.com');
+  // await page.injectFile('lib/hideEls.js');
+  // defaults.path = 'test/m.huffpost.com.jpg';
+  // await page.screenshot(defaults);
+
+  browser.close();
+});
