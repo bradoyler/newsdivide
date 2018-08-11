cb63039fed665a701a875763c9c6da356a2fbbfb
brad oyler <bradoyler@gmail.com>
2018-08-10 21:03:11 -0400

    upgrage to pupetteer 1.7

--- test/puppet.js
+++ test/puppet.js
@@ -10,17 +10,17 @@ puppeteer.launch().then(async browser => {
   const page = await browser.newPage()
   await page.setViewport({ width: 375, height: 667, isMobile: true })
   await page.goto('https://www.nbcnews.com/mach')
-  await page.injectFile('lib/hideEls.js')
+  await page.addScriptTag({path: 'lib/hideAds.js'})
   defaults.path = 'test/www.nbcnews.mach.jpg'
   await page.screenshot(defaults)
 
   // await page.goto('https://www.nbcnews.com');
-  // await page.injectFile('lib/hideEls.js');
+  // await page.addScriptTag({path: 'lib/hideAds.js'})
   // defaults.path = 'test/www.nbcnews.com.jpg';
   // await page.screenshot(defaults);
   //
   // await page.goto('http://m.huffpost.com');
-  // await page.injectFile('lib/hideEls.js');
+  // await page.addScriptTag({path: 'lib/hideAds.js'})
   // defaults.path = 'test/m.huffpost.com.jpg';
   // await page.screenshot(defaults);
 
